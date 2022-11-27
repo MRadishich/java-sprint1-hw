49c52b74fdd17dbad689ef70557375b4a585f01b
MRadishich <mradishich@gmail.com>
2022-11-27 23:44:24 +0300

    removal of coefficients from a methods "calculateDistance" and "calculateKilocalories"

--- src/Converter.java
+++ src/Converter.java
@@ -1,12 +1,12 @@
 public class Converter {
+    private double distanceOneStep = 0.00075; //Кол-во пройденных киллометров за 1 шаг
+    private double caloriesOneStep = 0.050; //Кол-во сжигаемых килокалорий за 1 шаг
 
     public double calculateDistance(double steps) {
-        double distanceOneStep = 0.00075; //Кол-во пройденных киллометров за 1 шаг
         return steps * distanceOneStep;
     }
 
     public double calculateKilocalories(int steps) {
-        double caloriesOneStep = 0.050; //Кол-во сжигаемых килокалорий за 1 шаг
         return steps * caloriesOneStep;
     }
 }
