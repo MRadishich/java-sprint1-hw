cbf5f15fc1e845a0ff42d12f1b18bbeec7ed6019
MRadishich <mradishich@gmail.com>
2022-12-03 23:27:32 +0300

    Code refactoring

---
+++ src/Month.java
@@ -0,0 +1,30 @@
+public enum Month {
+    JANUARY(31),
+    FEBRUARY(28),
+    MARCH(31),
+    APRIL(30),
+    MAY(31),
+    JUNE(30),
+    JULY(31),
+    AUGUST(31),
+    SEPTEMBER(30),
+    OCTOBER( 31),
+    NOVEMBER( 30),
+    DECEMBER(31);
+
+
+    private final int maxDays;
+
+    Month(int maxDays) {
+        this.maxDays = maxDays;
+    }
+
+    public static int getMaxDays(int i) {
+        for (Month month : values()) {
+            if (month.ordinal() == i) {
+                return month.maxDays;
+            }
+        }
+        return 0;
+    }
+}
